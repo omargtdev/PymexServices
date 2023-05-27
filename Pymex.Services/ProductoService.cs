4e7e199b390fdaf273919faec99dc8a98cceab71
omargtdev <omargtdev@gmail.com>
2023-05-27 09:51:30 -0500

    first commit: 3 services created

---
+++ Pymex.Services/ProductoService.cs
@@ -0,0 +1,25 @@
+﻿using Pymex.Services.Contracts;
+using Pymex.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Pymex.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductoService" in both code and config file together.
+    public class ProductoService : IProductoService
+    {
+        public string DoWork()
+        {
+            using(PymexEntities db  = new PymexEntities())
+            {
+                var query = (from producto in db.Producto select producto).FirstOrDefault().Descripcion;
+                return query;
+            }
+           
+        }
+    }
+}
---
+++ Pymex.Services/ProveedorService.cs
@@ -0,0 +1,18 @@
+﻿using Pymex.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Pymex.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProveedorService" in both code and config file together.
+    public class ProveedorService : IProveedorService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
